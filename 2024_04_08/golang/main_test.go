5be17e6dd4183bd46cc35e6fd9aa2755709b3f41
aparaxium <10635434+Aparaxium@users.noreply.github.com>
2024-04-17 23:13:18 -0700

    chore: rename folder to correct date

--- 2024_04_08/golang/main_test.go
+++ 2024_04_08/golang/main_test.go
@@ -4,28 +4,28 @@ import (
 	"testing"
 )
 
-func test1(t *testing.T) {
+func Test1(t *testing.T) {
 	result := p1([]int{10, 15, 3, 7}, 17)
 	if !result {
 		t.Fatalf("Expected true %t", result)
 	}
 }
 
-func test2(t *testing.T) {
+func Test2(t *testing.T) {
 	result := p1([]int{10, 15, 3, 7}, 16)
 	if result {
 		t.Fatalf("Expected false %t", result)
 	}
 }
 
-func test3(t *testing.T) {
+func Test3(t *testing.T) {
 	result := p1([]int{7}, 16)
 	if result {
 		t.Fatalf("Expected false %t", result)
 	}
 }
 
-func test4(t *testing.T) {
+func Test4(t *testing.T) {
 	result := p1([]int{}, 16)
 	if result {
 		t.Fatalf("Expected false %t", result)
